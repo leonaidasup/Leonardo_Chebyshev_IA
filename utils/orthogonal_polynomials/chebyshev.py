f628d309387d300de6ccf23279fd33ff40e10964
leonaidasup <111338890+leonaidasup@users.noreply.github.com>
2025-05-16 16:14:18 +0000

    feat: terminacion de la clase chebyshev

--- utils/orthogonal_polynomials/chebyshev.py
+++ utils/orthogonal_polynomials/chebyshev.py
@@ -1,4 +1,4 @@
-from numpy import cos, arccos
+from numpy import cos, arccos, zeros, ndarray
 
 class Chebyshev_polynomials:
     def __init__(self,
@@ -11,5 +11,16 @@ class Chebyshev_polynomials:
         self.interval: tuple[float, float] = interval
         self.ARRAY_omega: dict[int, float] = ARRAY_omega
     
-    def MAP_f(self, ARRAY_x):
-        result = []
+    def MAP_f(self, ARRAY_x: ndarray):
+        ARRAY_x = (2 * ARRAY_x - self.beta) / self.alpha
+        result: float = zeros(ARRAY_x.shape)
+        for i, omega in ARRAY_omega:
+            result += omega * cos(i * arccos(ARRAY_x))
+        return result
+    
+    def f(self, x: float) -> float:
+        x: float = (2 * x - self.beta) / self.alpha
+        result: float = 0.0
+        for i, omega in ARRAY_omega:
+            result += omega * cos(i * arccos(x))
+        return result
