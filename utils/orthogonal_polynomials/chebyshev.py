ab70ee6803f0022e4446b3d354db6235d3522482
Leonardo Jose Amaris Dominguez <leonardo.amaris@udea.edu.co>
2025-05-16 07:35:17 -0500

    Inicio de los polinomios de chebyshev

--- utils/orthogonal_polynomials/chebyshev.py
+++ utils/orthogonal_polynomials/chebyshev.py
@@ -1,2 +1,15 @@
+from numpy import cos, arccos
+
 class Chebyshev_polynomials:
-    pass
+    def __init__(self,
+                alpha: float,
+                beta: float,
+                interval: tuple[float, float],
+                ARRAY_omega: dict[int, float]) -> None:
+        self.alpha: float = alpha
+        self.beta: float = beta
+        self.interval: tuple[float, float] = interval
+        self.ARRAY_omega: dict[int, float] = ARRAY_omega
+    
+    def MAP_f(self, ARRAY_x):
+        result = []
